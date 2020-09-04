024843952bcf9ed6725bea39e803b436e5c8c7f5
thurgon <markus_moeller@hotmail.de>
2020-09-04 11:06:42 +0200

    update daemon and main, added sentry support, added config

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
+import sentry_sdk
+import json
+import platform
+import sys
 from bot import Bot
+from daemon import Daemon
 
-client = Bot()
-client.set_client(client)
-# client.run("NzUwMDI3MjEwNzcyOTcxNTQz.X00jRQ.48ATEDJsusMzOLdd6WDZ5ydnO6Q")
-client.run("NzQ5MzE0Mjc5Njg1MjI2NjE4.X0qLTQ.AF2YPp5kTnqTfxLG5YSsYmH9tMg")
+# load config and activate sentry if set.
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+if config is not None:
+    dsn = config.get('sentry_dsn')
+else:
+    dsn = ""
+
+# sentry
+sentry_sdk.init(
+    dsn,
+    traces_sample_rate=1.0
+)
+
+
+# set up deamon class:
+class BotDaemon(Daemon):
+    """Actual Daemon overwriting its parent run-method"""
+    def run(self):
+        daemon_client = Bot()
+        daemon_client.set_client(client)
+        # client.run("NzUwMDI3MjEwNzcyOTcxNTQz.X00jRQ.48ATEDJsusMzOLdd6WDZ5ydnO6Q")
+        daemon_client.run("NzQ5MzE0Mjc5Njg1MjI2NjE4.X0qLTQ.AF2YPp5kTnqTfxLG5YSsYmH9tMg")
+
+
+if __name__ == "__main__":
+    cur_os = platform.system()
+
+    if cur_os == "Linux":
+        daemon = BotDaemon('/tmp/transl8me.pid')
+        if len(sys.argv) == 2:
+            if 'start' == sys.argv[1]:
+                daemon.start()
+            elif 'stop' == sys.argv[1]:
+                daemon.stop()
+            elif 'restart' == sys.argv[1]:
+                daemon.restart()
+            else:
+                print("Unknown command")
+                sys.exit(2)
+            sys.exit(0)
+        else:
+            print("usage: %s start|stop|restart" % sys.argv[0])
+            sys.exit(2)
+
+    elif cur_os == "Windows":
+        # bot
+        client = Bot()
+        client.set_client(client)
+
+        # client.run("NzUwMDI3MjEwNzcyOTcxNTQz.X00jRQ.48ATEDJsusMzOLdd6WDZ5ydnO6Q")
+        client.run("NzQ5MzE0Mjc5Njg1MjI2NjE4.X0qLTQ.AF2YPp5kTnqTfxLG5YSsYmH9tMg")
+
+    else:
+        print("could not recognize OS")
--- translation.py
+++ translation.py
@@ -3,17 +3,20 @@ from googletrans import Translator
 
 
 class TranslateMe:
+    """Class supplying translation related methods"""
     def __init__(self):
         pass
 
     @staticmethod
     def get_language(text):
+        """method to extract the language of a given text"""
         lang = TextBlob(text)
         result = lang.detect_language()
         return result
 
     @staticmethod
     def translate_text(text, dest_language):
+        """method to translate a given text based on the supplied language code"""
         translate = Translator()
         result = translate.translate(text, dest_language)
         return result
